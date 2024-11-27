95636185144efe18276b88fb27180ef2f5bdd446
diouri11 <adamdiouri2@gmail.com>
2024-11-27 22:25:58 +0100

    27/11/25

---
+++ N*.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+N = int(input("numer of *"));
+for i in range(1, N):
+    for y in range(1,N):
+        print ("*");
+    print("\n");
+
+"""    Énoncé :
+Demandez à l’utilisateur d’entrer un nombre N. Affichez un carré d’étoiles (*).
+Exemple pour N = 3 :***
+                    ***
+                    ***
+
+"""
